1a1f5288637e9f55eca46b7e1a9df49e4abf7b7b
johnsmith20200203 <john.smith.20200203@gmail.com>
2022-03-27 11:30:24 +0900

    rails g実行時に作成、更新しないファイルを指定する

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Bookers2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.assets false
+      g.test_framework false
+      g.helper false
+      g.skip_routes true
+    end
   end
 end
