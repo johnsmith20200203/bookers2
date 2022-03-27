e8a8fb7e0c529b7a7c5885a3e1f6eeb6112320ce
johnsmith20200203 <john.smith.20200203@gmail.com>
2022-03-27 16:13:21 +0900

    User info表示部分の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_user
 
   def after_sign_in_path_for(resource)
     user_path(resource)
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 
   protected
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def index
   end
   def show
+    # WARNING: user詳細画面ではcurrent_userではなく対象のUser情報を@userに上書きしている
+    @user = User.find(params[:id])
   end
 end
