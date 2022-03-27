308b48e82fb8e308139bccb62b74c8ceac75055a
johnsmith20200203 <john.smith.20200203@gmail.com>
2022-03-27 13:31:47 +0900

    aboutページ用のcontrollerとviewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#index'
+  get '/about', to: 'about#index', as: 'about'
   get '/users/:id', to: 'users#show', as: 'user'
 end
